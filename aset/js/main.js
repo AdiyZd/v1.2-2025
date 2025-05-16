37c93b301465912f5b429844f4a4cbaaa0129684
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2025-05-16 10:48:40 +0700

    update page
    update

--- aset/js/main.js
+++ aset/js/main.js
@@ -1,18 +1,23 @@
 const darkModeToggle = document.getElementById("darkModeToggle");
 const body = document.body;
 const navbar = document.querySelector(".navbar");
+let collapse = document.getElementById("collapseExample");
+let arrowIcon = document.getElementById("arrowIcon");
 
 // cek preferensi dark mode
-const preferensiDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-const mode = localStorage.getItem("darkMode") || (preferensiDark ? "dark" : "light");
+const preferensiDark = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+const mode =
+  localStorage.getItem("darkMode") || (preferensiDark ? "dark" : "light");
 
 // Mode pertama kali dokument dibuka
 if (mode === "dark") {
   body.classList.add("dark-mode");
   darkModeToggle.innerHTML = '<i class="bi bi-sun-fill"></i>';
 } else {
-    body.classList.remove("dark-mode");
-    darkModeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
+  body.classList.remove("dark-mode");
+  darkModeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
 }
 
 // saat tombol darkmode di klik
@@ -27,4 +32,15 @@ darkModeToggle.addEventListener("click", () => {
     localStorage.setItem("darkMode", "light");
     darkModeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
   }
-})
+});
+
+// ini untuk drop down
+collapse.addEventListener("show.bs.collapse", () => {
+  arrowIcon.classList.remove("bi-chevron-down");
+  arrowIcon.classList.add("bi-chevron-up");
+});
+
+collapse.addEventListener("hide.bs.collapse", () => {
+  arrowIcon.classList.remove("bi-chevron-up");
+  arrowIcon.classList.add("bi-chevron-down");
+});
